9f33140125ca73aa7bfd22a9ca35c9de1640160c
Twalter18 <78177545+Twalter18@users.noreply.github.com>
2021-05-02 03:07:17 -0400

    Final Project

---
+++ app/src/main/java/com/bignerdranch/android/finalprojectcriminalintent/CrimeListViewModel.kt
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.finalprojectcriminalintent
+
+import androidx.lifecycle.ViewModel
+
+class CrimeListViewModel : ViewModel() {
+
+    private val crimeRepository = CrimeRepository.get()
+    val crimeListLiveData = crimeRepository.getCrimes()
+
+    fun addCrime(crime: Crime) {
+        crimeRepository.addCrime(crime)
+    }
+}
